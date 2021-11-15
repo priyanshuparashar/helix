22429dc74acc8f711a44e5464022493c4a0d5db0
priyanshu parashar <priyanshuparashar223@gmail.com>
2021-11-15 12:04:33 +0530

    models created of sales

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Companies, Sales, Products
 
 # Register your models here.
+admin.site.register(Companies)
+admin.site.register(Sales)
+admin.site.register(Products)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Companies(models.Model):
+    c_name = models.CharField(max_length=200)
+    c_email = models.EmailField(null=False)
+    created=models.DateTimeField(auto_now_add=True)
+    
+    
+    
+    
+    def __str__(self):
+        return self.c_name
+    
+class Products(models.Model):
+    p_name=models.CharField(max_length=400, null=False)
+    p_amount=models.CharField(null=True, max_length=300)
+    def __str__(self):
+        return self.p_name
+    
+CHOICES = (
+    ("fail", "fail"),
+    ("mid", "mid"),
+    ("done", "done"),
+   
+)
+
+class Sales(models.Model):
+    company_name = models.ForeignKey(Companies, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Products, on_delete=models.SET_NULL, null=True)
+    state = models.CharField(
+        max_length = 50,
+        choices = CHOICES,
+        default = 'fail'
+        )
+    def __str__(self):
+        return self.state
+
